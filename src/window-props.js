283047b46c193451e44e63240819babba893fe8b
joncasey <joncasey@gmail.com>
2017-12-29 15:39:31 -0500

    add 'c' as shorthand for centering window properties.

--- src/window-props.js
+++ src/window-props.js
@@ -4,7 +4,17 @@ function windowProps (r, m) {
     return s.replace(/w/g, screen.availWidth).replace(/h/g, screen.availHeight)
             .split(' ').map(function (v) { return Function('return ' + v)() })
   }
-  if (r) r = x(r), resizeTo(r[0], r[1])
-  if (m) m = x(m), moveTo(m[0], m[1])
+  if (r) {
+    r = x(r)
+    resizeTo(r[0], r[1])
+  }
+  if (m) {
+    m = m
+      .replace(/^c$/, 'c c')
+      .replace(/^c /, '(w-' + r[0] + ')/2 ')
+      .replace(/ c$/, ' (h-' + r[1] + ')/2')
+    m = x(m)
+    moveTo(m[0], m[1])
+  }
 }
 
