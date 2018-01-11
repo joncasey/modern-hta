864ac6c5c06df8b651f8745933d315725dae6796
joncasey <joncasey@gmail.com>
2018-01-10 22:23:19 -0500

    use regexp for getSync() test this allows File to be sent directly

--- src/get-sync.js
+++ src/get-sync.js
@@ -3,8 +3,11 @@ var xhr = new ActiveXObject('Msxml2.XMLHTTP.6.0')
 var sys = new ActiveXObject('Wscript.Shell')
 var cwd = sys.CurrentDirectory
 
-var getSync = function (url) {
-  if (url.indexOf(':') === -1) url = cwd + '/' + url
-  xhr.open('GET', url, false); xhr.send()
+function getSync (url) {
+  if (!/:/.test(url)) {
+    url = cwd + '/' + url
+  }
+  xhr.open('GET', url, false)
+  xhr.send()
   return xhr.responseText
 }
